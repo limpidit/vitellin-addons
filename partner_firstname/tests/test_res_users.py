25915305a5123ae7b0abf4c8ded603faab4a4793
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2025-05-06 11:56:51 +0200

    fix - Monkey patching du test qui échoue

--- partner_firstname/tests/test_res_users.py
+++ partner_firstname/tests/test_res_users.py
@@ -1,26 +1,25 @@
 
-from odoo.addons.base.tests.test_res_users import TestUsers2
-from odoo.tests.common import tagged
+from odoo.addons.base.tests import test_res_users
 from odoo.tests import Form
 
+def patched_test_reified_groups(self):
+    """ The groups handler doesn't use the "real" view with pseudo-fields
+    during installation, so it always works (because it uses the normal
+    groups_id field).
+    """
+    # use the specific views which has the pseudo-fields
+    f = Form(self.env['res.users'], view='base.view_users_form')
+    f.firstname = "bob"
+    f.lastname = "test"
+    f.login = "bob"
+    user = f.save()
 
-@tagged('standard', '-at_install')
-class TestUsers2Patched(TestUsers2):
+    self.assertIn(self.env.ref('base.group_user'), user.groups_id)
 
-    def test_reified_groups(self):
-        """ The groups handler doesn't use the "real" view with pseudo-fields
-        during installation, so it always works (because it uses the normal
-        groups_id field).
-        """
-        # use the specific views which has the pseudo-fields
-        f = Form(self.env['res.users'], view='base.view_users_form')
-        f.firstname = "bob"
-        f.lastname = "test"
-        f.login = "bob"
-        user = f.save()
+    # all template user groups are copied
+    default_user = self.env.ref('base.default_user')
+    self.assertEqual(default_user.groups_id, user.groups_id)
 
-        self.assertIn(self.env.ref('base.group_user'), user.groups_id)
+# Monkey patch du test directement
+test_res_users.TestUsers2.test_reified_groups = patched_test_reified_groups
 
-        # all template user groups are copied
-        default_user = self.env.ref('base.default_user')
-        self.assertEqual(default_user.groups_id, user.groups_id)
