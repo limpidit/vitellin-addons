a014fc1c6a45913b8e1ed421debbc455356bf408
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2025-05-16 16:37:37 +0200

    migration - cap_project_editions, cap_project_task_filter

---
+++ cap_project_editions/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "cap_project_editions",
+    'summary': "Définition des éditions projet.",
+    'description': "Définition des éditions projet.",
+    'author': "Captivea",
+    'website': "https://www.captivea.com",
+    'category': 'Tools',  # La catégorie reste la même
+    'version': '17.0.1.0.0',  # Mise à jour de la version pour Odoo 17
+    'depends': ['cap_project', 'cap_industry_fsm', 'cap_sale', 'cap_sale_stock'],
+    'data': [
+        'data/wizard_feuille_journee/report.xml',
+        'data/wizard_feuille_journee/ir_actions_report.xml',
+        'data/wizard_feuille_semaine/ir_model_access.xml',
+        'data/wizard_feuille_semaine/ir_ui_view.xml',
+        'data/wizard_feuille_semaine/report.xml',
+        'data/wizard_feuille_semaine/ir_actions_report.xml',
+    ],
+    'qweb': [],
+    'demo': [],
+    'installable': True,  # Ajout pour permettre l'installation du module
+}
---
+++ cap_project_editions/models/__init__.py
@@ -0,0 +1,2 @@
+from . import project_task
+from . import wizard_feuille_semaine
