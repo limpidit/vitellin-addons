62f0af5d23c681b1d4a18df95c45bab5990ddd83
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2025-05-16 16:32:41 +0200

    migration - cap_project_process

---
+++ cap_project_process/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap_project_process",
+    'summary': "Définition des processus projet.",
+    'description': "Définition des processus projet.",
+    'author': "Captivea",
+    'website': "https://www.captivea.com",
+    'category': 'Tools',
+    'version': '17.0.1.0',
+    'depends': ['cap_project', 'cap_industry_fsm', 'cap_sale', 'cap_account'],
+    'data': [],
+    'qweb': [],
+    'demo': [],
+}
