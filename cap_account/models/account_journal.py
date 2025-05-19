46e310b33cec31de23b145e33d2a521bc529b917
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-05-19 16:18:50 +0200

    fix - unlock journal

--- cap_account/models/account_journal.py
+++ cap_account/models/account_journal.py
@@ -10,14 +10,7 @@ class Journal(models.Model):
         if self.restrict_mode_hash_table:
             journal_entries = self.env['account.move'].search([('journal_id', '=', self.id), ('restrict_mode_hash_table', '=', True)])
 
-            cr = self.env.cr
-            # Supprimer le verrouillage sur le journal
-            cr.execute('''UPDATE account_journal SET restrict_mode_hash_table=false WHERE id=%s''', (self.id,))
-            # Invalider le cache
-            self.flush()
-            self.invalidate_cache()
-            # Invalider le cache pour les factures impactées
-            journal_entries.flush()
-            journal_entries.invalidate_cache()
+            self.sudo().write({'restrict_mode_hash_table': False})
+            journal_entries.invalidate_model()
         else:
             raise UserError("Opération non autorisée. \nLe journal n'est pas verrouillé !")
