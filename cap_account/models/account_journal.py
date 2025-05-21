40c9c0b5de7cfe753342850cff925c40a01141e9
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-05-21 12:04:53 +0200

    fix - Impossibilité de déverouiller le journal

--- cap_account/models/account_journal.py
+++ cap_account/models/account_journal.py
@@ -1,16 +1,20 @@
 from odoo import models
+from odoo.addons.account.models.account_journal import AccountJournal
 from odoo.exceptions import UserError
 
 
 class Journal(models.Model):
     _inherit = 'account.journal'
 
+    def write(self, vals):
+        if self.env.context.get('force_unlock_hash'):
+            return super(AccountJournal, self).write(vals)
+        return super().write(vals)
+
     def action_unlock_journal(self):
         self.ensure_one()
         if self.restrict_mode_hash_table:
-            journal_entries = self.env['account.move'].search([('journal_id', '=', self.id), ('restrict_mode_hash_table', '=', True)])
-
-            self.sudo().write({'restrict_mode_hash_table': False})
-            journal_entries.invalidate_model()
+            self.with_context(force_unlock_hash=True).write({'restrict_mode_hash_table': False})
+            return
         else:
             raise UserError("Opération non autorisée. \nLe journal n'est pas verrouillé !")
