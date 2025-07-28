72f359ce24f0dae5a2fe130588df0063c518640c
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2025-07-28 11:07:53 +0200

    fix je sais pas c quoi lerreur

--- cap_industry_fsm/__manifest__.py
+++ cap_industry_fsm/__manifest__.py
@@ -22,15 +22,15 @@
         'data/project_task/ir_ui_menu.xml',
         'data/project_task/ir_sequence.xml',
     ],
-    'qweb': [
-        'static/src/xml/MapView.xml',
-    ],
-    'assets': {
-        'web.assets_backend': [
-            'cap_industry_fsm/static/src/js/map_renderer.js',
-            'cap_industry_fsm/static/src/css/mapview.css',
-        ],
-    },
+    # 'qweb': [
+    #     'static/src/xml/MapView.xml',
+    # ],
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'cap_industry_fsm/static/src/js/map_renderer.js',
+    #         'cap_industry_fsm/static/src/css/mapview.css',
+    #     ],
+    # },
 
     'demo': [],
     'installable': True,  # Assure que le module peut être installé
